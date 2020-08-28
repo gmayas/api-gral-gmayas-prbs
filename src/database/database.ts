fdb0135a49d46dd9cb65b69d0c72f4de48fdf0d7
Gabiel Maya Sanchez <isc_gmayas@hotmail.com>
2020-08-27 20:44:33 -0500

    make it better5

--- src/database/database.ts
+++ src/database/database.ts
@@ -6,9 +6,9 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const connectionString = `postgresql://${process.env.User}:${process.env.Password}@${process.env.Host}:${process.env.Port}/${process.env.Database}`
 
 const pool = new Pool({
-  //connectionString: isProduction ? process.env.URI : connectionString,
-  connectionString: process.env.URI,
-  ssl: true,
+  connectionString: isProduction ? process.env.URI : connectionString,
+  //connectionString: process.env.URI,
+  ssl: true
 })
 
 export default pool;
